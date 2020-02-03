b2a42776b59d020cf525504ae1056843408fdbe1
Willian Werner <willian.werner@infosimples.com.br>
2020-02-03 15:54:55 -0300

    CLI

--- datasus/database.py
+++ datasus/database.py
@@ -9,12 +9,19 @@ import glob
 import os
 import re
 
+def prepare_environment():
+    dbc2dbf_filename = os.path.join("datasus", "scripts", "dbc2dbf")
+    if not os.path.exists(dbc2dbf_filename):
+        subprocess.run([
+            "gcc", "-o", dbc2dbf_filename,
+            os.path.join("datasus", "scripts", "dbc2dbf.c"),
+            os.path.join("datasus", "scripts", "blast.c")])
 
-def dbc2bdf_single(filename):
+def dbc2dbf_single(filename):
     metadata = get_metadata(filename)
-    converted_file = f'{metadata["path"]}.bdf'
+    converted_file = f'{metadata["path"]}.dbf'
     subprocess.run([
-        os.path.join('datasus', 'scripts', 'dbc2bdf'),
+        os.path.join('datasus', 'scripts', 'dbc2dbf'),
         filename,
         converted_file
     ])
@@ -28,16 +35,16 @@ def extract_files(data_folder, filters):
     data_files = glob.glob(
         os.path.join(data_folder, '*.dbc'))
     for filename in tqdm(data_files):
-        converted = dbc2bdf_single(filename)
+        converted = dbc2dbf_single(filename)
         db = converted['database']
         database = os.path.join(database_folder, db)
         try:
-            bdf = list(DBF(converted['filename']))
+            dbf = list(DBF(converted['filename']))
         except ValueError:
             corrupted_files.append(converted['filename'])
-        if not bdf:
+        if not dbf:
             continue
-        df = pd.DataFrame(bdf, columns=bdf[0].keys())
+        df = pd.DataFrame(dbf, columns=dbf[0].keys())
         df['month'] = converted['month']
         df['year'] = converted['year']
         df['uf'] = converted['uf']
